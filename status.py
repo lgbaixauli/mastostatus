999804b2f7da890ca95bc9f6b1d43188d09e6625
lgbaixauli <lgbaixauli@gmail.com>
2022-12-12 23:18:04 +0100

    [202212122318]

--- status.py
+++ status.py
@@ -10,41 +10,22 @@ from bundle.config import Config
 from bundle.logger import Logger
 from bundle.translator import Translator
 
-import yaml
 import random
 
-class Runner:
-    '''
-    Main runner of the app
-    '''
-    def init(self):
-        self._config     = Config()
-        self._logger     = Logger(self._config).getLogger()
+BOT_NAME = "Statusbot"
+
+class Bot(Mastobot):
+
+    def __init__(self, botname: str = BOT_NAME) -> None:
 
         self._translator = Translator("es")
-        self._bot        = Mastobot(self._config)
 
-        self.init_app_options()
-        self.init_test_options()
-
-        self._logger.info("init app")
-
-        return self
-
-
-    def init_app_options(self):
-
-        actions_file_name = self._config.get("app.actions_file_name") 
-        with open(actions_file_name, 'r') as stream:
-            self._actions  = yaml.safe_load(stream)
-        
-        self._logger.debug ("len actions: "   + str(len(self._actions)))        
+        super().__init__(botname = botname)
 
 
     def init_test_options (self):
 
-        self._dismiss_disable = self._config.get("testing.disable_dismiss_notification")
-        self._push_disable    = self._config.get("testing.disable_push_answer")
+        self._dismiss_disable = self._config.get("testing.disable_dismiss")
         self._test_word       = self._config.get("testing.text_word")
 
         self._logger.debug ("test_word: "   + self._test_word)
@@ -55,12 +36,12 @@ class Runner:
         else:
             self._ignore_test = False 
 
+        super().init_test_options()
+ 
 
-    def run(self):
+    def run(self, botname: str = BOT_NAME) -> None:
 
-        self._logger.debug ("runing app")
-
-        notifications = self._bot.mastodon.notifications()
+        notifications = self.mastodon.notifications()
 
         for notif in notifications:
 
@@ -70,61 +51,59 @@ class Runner:
 
                 if self._dismiss_disable:
                     dismiss = False
-                    self._logger.debug("dismissing disabled notification id" + str(notif.id))                    
+                    self._logger.debug("dismissing disabled notification id " + str(notif.id))                    
     
-                if self._ignore_test and self._bot.check_keyword_in_nofit(self._bot, notif, self._test_word):
+                if self._ignore_test and self.check_keyword_in_nofit(notif, self._test_word):
                     dismiss = False
-                    self._logger.info("ignoring test notification id" + str(notif.id))
+                    self._logger.info("ignoring test notification id " + str(notif.id))
                 else: 
 
-                    for action in self._actions:
+                    action = self._actions ["instance_status"]
 
-                        if self._bot.check_keyword_in_nofit(self._bot, notif, action["keyword"]):
-                            text_post = self.replay_text(notif.status.language, action)
+                    if self.check_keyword_in_nofit(notif, action["keyword"]):
+                        text_post = self.find_text(notif.status.language, action["keyword"])
 
-                            if self._push_disable:
-                                self._logger.info("pushing answer disabled notification id" + str(notif.id))                     
-                            else:
-                                self._logger.info("answering notification id" + str(notif.id))
-                                self._bot.replay(notif, text_post)
+                        if self._push_disable:
+                            self._logger.info("pushing answer disabled notification id " + str(notif.id))                     
+                        else:
+                            self._logger.info("answering notification id " + str(notif.id))
+                            self.replay(notif, text_post)
 
             if dismiss:
-                self._logger.debug("dismissing notification id" + str(notif.id))
-                self._bot.mastodon.notifications_dismiss(notif.id)
+                self._logger.debug("dismissing notification id " + str(notif.id))
+                self.mastodon.notifications_dismiss(notif.id)
 
-        self._logger.info("end app")
+        super().run(botname = botname)
 
 
-    def replay_text(self, language, action):        
+    def find_text(self, language, keyword):        
     
-        keyword = action["keyword"]
-
         self._logger.debug("notif language: " + language)                    
         self._logger.debug("notif kewword : " + keyword) 
 
         self._translator.fix_language (language)
         _text     = self._translator.get_text
         
-        mau = self._bot.mastodon.instance_nodeinfo().usage.users.activeMonth
+        mau = self.mastodon.instance_nodeinfo().usage.users.activeMonth
         mau = '{:,}'.format(mau).replace(',','.')
 
-        registers = self._bot.mastodon.instance().stats.user_count
+        registers = self.mastodon.instance().stats.user_count
         registers = '{:,}'.format(registers).replace(',','.')
 
-        posts = self._bot.mastodon.instance().stats.status_count
+        posts = self.mastodon.instance().stats.status_count
         posts = '{:,}'.format(posts).replace(',','.')
 
-        peers = self._bot.mastodon.instance().stats.domain_count
+        peers = self.mastodon.instance().stats.domain_count
         peers = '{:,}'.format(peers).replace(',','.')
 
-        version = self._bot.mastodon.instance().version
+        version = self.mastodon.instance().version
 
-        if self._bot.mastodon.instance_nodeinfo().openRegistrations:
+        if self.mastodon.instance_nodeinfo().openRegistrations:
             opened = _text ("abierto")
         else:
             opened = _text ("cerrado")
 
-        post_text  = ", " +_text("estado") + " " + self._bot._hostname + ":\n\n"
+        post_text  = ", " +_text("estado") + " " + self._hostname + ":\n\n"
         post_text += _text("registrados") + ": " + registers + "\n"
         post_text += _text("activos") + ": " + mau + "\n"
         post_text += _text("apuntes") + ": " + posts + "\n"
@@ -139,7 +118,8 @@ class Runner:
 
         return post_text
 
+
 # main
 
 if __name__ == '__main__':
-    Runner().init().run()
+    Bot().run()
