a45ad8caffebc8b0d927a6181720e630d96e98cb
lgbaixauli <lgbaixauli@gmail.com>
2022-12-13 23:59:16 +0100

    [202212132359]

--- status.py
+++ status.py
@@ -18,26 +18,11 @@ class Bot(Mastobot):
 
     def __init__(self, botname: str = BOT_NAME) -> None:
 
-        self._translator = Translator("es")
-
         super().__init__(botname = botname)
 
+        self.init_replay_bot()
+        self.init_translator()
 
-    def init_test_options (self):
-
-        self._dismiss_disable = self._config.get("testing.disable_dismiss")
-        self._test_word       = self._config.get("testing.text_word")
-
-        self._logger.debug ("test_word: "   + self._test_word)
-        self._logger.debug ("ignore test: " + str(self._config.get("testing.ignore_test_toot")))
-
-        if self._test_word != "" and self._config.get("testing.ignore_test_toot"):
-            self._ignore_test = True
-        else:
-            self._ignore_test = False 
-
-        super().init_test_options()
- 
 
     def run(self, botname: str = BOT_NAME) -> None:
 
@@ -45,39 +30,23 @@ class Bot(Mastobot):
 
         for notif in notifications:
 
-            dismiss = True
-
-            if notif.type == 'mention':
-
-                if self._dismiss_disable:
-                    dismiss = False
-                    self._logger.debug("dismissing disabled notification id " + str(notif.id))                    
-    
-                if self._ignore_test and self.check_keyword_in_nofit(notif, self._test_word):
-                    dismiss = False
-                    self._logger.info("ignoring test notification id " + str(notif.id))
-                else: 
-
-                    action = self._actions ["instance_status"]
-
-                    if self.check_keyword_in_nofit(notif, action["keyword"]):
-                        text_post = self.find_text(notif.status.language, action["keyword"])
-
-                        if self._push_disable:
-                            self._logger.info("pushing answer disabled notification id " + str(notif.id))                     
-                        else:
-                            self._logger.info("answering notification id " + str(notif.id))
-                            self.replay(notif, text_post)
-
+            action   = self._actions["instance_status"]   
+            replay, dismiss = self.process_notif(notif, "mention", action["keyword"])
+            if replay:
+                self.replay_toot(self.find_text(notif, action), notif)
+     
             if dismiss:
-                self._logger.debug("dismissing notification id " + str(notif.id))
                 self.mastodon.notifications_dismiss(notif.id)
 
         super().run(botname = botname)
 
 
-    def find_text(self, language, keyword):        
-    
+    def find_text(self, notif, action):        
+
+        language = notif.status.language
+        username = notif.account.acct
+        keyword  = action["keyword"]
+        
         self._logger.debug("notif language: " + language)                    
         self._logger.debug("notif kewword : " + keyword) 
 
@@ -103,7 +72,7 @@ class Bot(Mastobot):
         else:
             opened = _text ("cerrado")
 
-        post_text  = ", " +_text("estado") + " " + self._hostname + ":\n\n"
+        post_text  = "@" + username + ", " +_text("estado") + " " + self._hostname + ":\n\n"
         post_text += _text("registrados") + ": " + registers + "\n"
         post_text += _text("activos") + ": " + mau + "\n"
         post_text += _text("apuntes") + ": " + posts + "\n"
