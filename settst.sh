ea21bbb6a20b36f78dca53bbf306c9ce4078f81b
lgbaixauli <lgbaixauli@gmail.com>
2022-12-18 23:15:08 +0100

    [202212182315]

--- settst.sh
+++ settst.sh
@@ -5,6 +5,8 @@ cd /home/mastostatus/mastostatus/
 sed -i 's/ignore_test_toot: True/ignore_test_toot: False/g' config.yaml
 sed -i 's/disable_post: False/disable_post: True/g' config.yaml
 sed -i 's/disable_dismiss: False/disable_dismiss: True/g' config.yaml
+sed -i 's/force_programmer: False/force_programmer: True/g' config.yaml
+sed -i 's/disable_write: False/disable_write: True/g' config.yaml
 sed -i 's/loglevel: 20/loglevel: 10/g' config.yaml
           
 
