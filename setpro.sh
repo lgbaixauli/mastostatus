1492d125edacd9ce1eb452c8d74ab69cba2305b6
lgbaixauli <lgbaixauli@gmail.com>
2022-12-23 19:02:57 +0100

    [202212231903]

--- setpro.sh
+++ setpro.sh
@@ -7,4 +7,7 @@ sed -i 's/disable_post: True/disable_post: False/g' config.yaml
 sed -i 's/disable_dismiss: True/disable_dismiss: False/g' config.yaml
 sed -i 's/force_programmer: True/force_programmer: False/g' config.yaml
 sed -i 's/disable_write: True/disable_write: False/g' config.yaml
+sed -i 's/force_mention: True/force_mention: False/g' config.yaml
+sed -i 's/user_mention: \"kergozh\"/user_mention: \"none\"/g' config.yaml
+
 sed -i 's/loglevel: 10/loglevel: 20/g' config.yaml
--- settst.sh
+++ settst.sh
@@ -7,6 +7,9 @@ sed -i 's/disable_post: False/disable_post: True/g' config.yaml
 sed -i 's/disable_dismiss: False/disable_dismiss: True/g' config.yaml
 sed -i 's/force_programmer: False/force_programmer: True/g' config.yaml
 sed -i 's/disable_write: False/disable_write: True/g' config.yaml
+sed -i 's/force_mention: False/force_mention: True/g' config.yaml
+sed -i 's/user_mention: \"none\"/user_mention: \"kergozh\"/g' config.yaml
+
 sed -i 's/loglevel: 20/loglevel: 10/g' config.yaml
           
 
